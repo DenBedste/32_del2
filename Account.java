5e08d0288aa435dfb226e4b47b12ed946c004534
denzildk <32563613+denzildk@users.noreply.github.com>
2017-10-27 12:15:30 +0200

    Konto to english
    oversat konto klassen fra dansk til engelsk.
    Translated class:konto->account to english

---
+++ Account.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+
+/**
+ * @author Stonecore
+ *
+ */
+public class Account {
+	int Balance, defaultBalance = 1000;
+
+	/**
+	 * @param args
+	 */
+	// constructor, sætter spillerens penge til startbeløbet som næmt kan ændres
+
+	public account() {
+		Balance = defaultBalance;
+		balanceCheck();
+		// TODO Auto-generated method stub
+
+	}
+	// setPenge tilføjer en int værdi til spilleren penge, hvis slutværdien bliver
+	// under 0 ændres den til 0
+
+	public void addBalance(int penge) {
+
+		this.Balance += Balance;
+		balanceCheck();
+
+	}
+
+	// sætter penge til 0 hvis den er under 0, kald denne metode efter alle
+	// ændringer i en spillers penge
+	private void balanceCheck() {
+		if (this.Balance < 0)
+			this.Balance = 0;
+	}
+
+}
