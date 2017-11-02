83cca9eb3c799b0376b51979c6a81a9e432995b9
DenBedste <32563463+DenBedste@users.noreply.github.com>
2017-11-02 14:25:39 +0100

    Changed slightly in game

--- src/cdio2_game/Game.java
+++ src/cdio2_game/Game.java
@@ -3,8 +3,6 @@ package cdio2_game;
 public class Game {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
-
 }
--- src/cdio2_game/Player.java
+++ src/cdio2_game/Player.java
@@ -1,8 +1,10 @@
+package cdio2_game;
 
-public class Player {
+public class Player extends Account {
 
   String playerName;
   
+  //Konstruktør
   public Player (String name, int playerAccount) {
     name = playerName;
     playerAccount = Balance;
