bf825073e79bdb90aed4eb6674e27b04fe0e73c5
Tahany <Tahany@tahanys-mbp.eduroam.wireless.dtu.dk>
2017-11-04 00:17:32 +0100

    små rettelser

--- src/cdio2_game/Account.java
+++ src/cdio2_game/Account.java
@@ -5,41 +5,40 @@ package cdio2_game;
  *
  */
 public class Account {
-	int Balance, defaultBalance = 1000;
+	int balance, defaultBalance = 1000;
 
 	/**
 	 * @param args
 	 */
 	// constructor, sætter spillerens penge til startbeløbet som næmt kan ændres
 
-	public Account(int Balance) {
-		this.Balance = defaultBalance;
+	public Account(int balance) {
+		this.balance = defaultBalance;
 		balanceCheck();
 
 	}
 	// retunere balancen i kontoen
 	public int getBalance() {
-		return Balance;
+		return balance;
 	}
 
 	// setPenge tilføjer en int værdi til spilleren penge, hvis slutværdien bliver
 	// under 0 ændres den til 0
 
 	public void addToBalance(int Balance) {
-
-		this.Balance += Balance;
+		this.balance += balance;
 		balanceCheck();
-
 	}
 
 	// sætter penge til 0 hvis den er under 0, kald denne metode efter alle
 	// ændringer i en spillers penge
 	private void balanceCheck() {
-		if (this.Balance < 0)
-			this.Balance = 0;
+		if (this.balance < 0)
+			this.balance = 0;
 	}
+	
 	public String toString() {
-		return  Balance + "";
+		return  balance + "";
 	}
 
 }
