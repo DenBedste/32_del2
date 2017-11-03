44eeac58c1cf5aa17a9661081aac5576210672f9
TahanyNemer1 <32563912+TahanyNemer1@users.noreply.github.com>
2017-11-03 12:12:41 +0100

    Update Player.java

--- src/cdio2_game/Player.java
+++ src/cdio2_game/Player.java
@@ -9,5 +9,26 @@ public class Player extends Account {
     name = playerName;
     playerAccount = Balance;
   }
+  // returns the name of the player
+	public String getName() {
+		return name;
+	}
+
+	// returns the money the player has
+	public int getBalance() {
+		return playerAccount.getBalance();
+	}
+
+	// adds money to the player account
+	public void addToBalance(int dbalance) {
+		this.playerAccount.addToBalance(dbalance);
+	}
+
+	// returns a String represents the player
+	public String toString() {
+		return name + "\t" + playerAccount + ".";
+	}
+}
+
   
 }
