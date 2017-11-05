a5903bfeb77632ab5804f14fe969021342e2f72b
Tahany <Tahany@192.168.0.10>
2017-11-05 22:20:47 +0100

    remove  playerAccount, getBalance and addToBalance methods

--- src/cdio2_game/Player.java
+++ src/cdio2_game/Player.java
@@ -1,48 +1,27 @@
 package cdio2_game;
 
 public class Player extends Account {
-
-  String playerName;
-  
-  /**
-   * Constructor
-   * @param name
-   * @param playerAccount
-   */
-  public Player (String name, int playerAccount) {
-    name = playerName;
-    playerAccount = Balance;
-  }
-  /**
-   * returns the name of the player
-   * @return
-   */
+	
+	String playerName;
+	
+	/**
+	 * 
+	 */
+	public Player (String name) {
+		playerName = name;
+	}
+	
+	/**
+	 * 
+	 */
 	public String getName() {
-		return name;
+		return playerName;
 	}
-
+	
 	/**
-	 *  returns the money the player has
-	 */
-	public int getBalance() {
-		return playerAccount.getBalance();
-	}
-
-	/**
-	 *  adds money to the player account
-	 * @param dbalance
-	 */
-	public void addToBalance(int dbalance) {
-		this.playerAccount.addToBalance(dbalance);
-	}
-
-	/**
-	 *  returns a String represents the player
+	 * 
 	 */
 	public String toString() {
-		return name + "\t" + playerAccount + ".";
+		return  playerName + "\t\t" + Balance + ".";
 	}
-}
-
-  
 }
