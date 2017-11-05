dd0931975df07973d1bf349c76dfa957abbbc275
Tahany <Tahany@192.168.0.10>
2017-11-05 22:35:10 +0100

    initializing and changes in methodes

--- src/cdio2_game/Cup.java
+++ src/cdio2_game/Cup.java
@@ -4,27 +4,50 @@ import java.util.Random;
 
 public class Cup {
 	private int[] eyes = new int[3];
+	private Random dice1,dice2;
 	private int sides;
-
-	/**
-	 * 	constructor of the class Cup
-	 */
-	public Cup(){
-		
-	}
+	
 	/**
 	 * 
+	 * @param sides
 	 */
-	
+	public Cup(int sides){
+		this.sides = sides;
+		dice1 = new Random();
+		dice2 = new Random();
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
 	public int rollCup() {
-		
+		rollDices();
+		eyes[2] = eyes[0] + eyes[1];
+		return eyes[2];
 	}
 	
+	/**
+	 * 
+	 * @param number
+	 * @return
+	 */
 	public int getEyes(int number) {
 		return eyes[number];
 	}
 
-	private void Dice() {
-		
+	/**
+	 * 
+	 */
+	private void rollDices() {
+		eyes[0] = dice1.nextInt(sides) + 1;
+		eyes[1] = dice2.nextInt(sides) + 1;
+	}
+
+	/**
+	 * 
+	 */
+	public String toString() {
+		return "( " + eyes[0] + " + " + eyes[1] + " ) = " + eyes[2] + ".";
 	}
 }
