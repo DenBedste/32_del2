9caedaf9177643bc5fd17ff62e256787814a016a
denzildk <hedegaard.cornelius@gmail.com>
2017-11-06 17:59:37 +0100

    finished player

--- src/cdio2_game/Player.java
+++ src/cdio2_game/Player.java
@@ -1,48 +1,46 @@
 package cdio2_game;
 
-
 public class Player {
-// Creating a String obj represents the player name
-		private String name;
-		// creating an int variable represents the player money account
-		private Account playerAccount;
-/**
- * 
- * @param name
- * @param balance
- */
-		// the constructor of the Class takes two parameters String name and int money
-		public Player() {
-playerAccount = new Account();
+	// Creating a String obj represents the player name
+	private String name;
+	// creating an int variable represents the player money account
+	private Account account;
 
-
-		}
-
-		// returns the name of the player
-		public String getName() {
-			return name;
-		}
-
-		// returns the money the player has
-		public int getBalance() {
-			return playerAccount.getBalance();
-		}
-
-		// adds money to the player account
-		public void addBalance(int dbalance) {
-			balance += dbalance;
-		}
-
-		// returns a String represents the player
-		public String toString() {
-			return name + "\t" + playerAccount + ".";
-		}
-
-		public void setName(Object println) {
-			// TODO Auto-generated method stub
-			
-		}
+	/**
+	 * 
+	 * @param name
+	 * @param balance
+	 */
+	// the constructor of the Class takes two parameters String name and int money
+	public boolean winner() {
+		if (account.getBalance() >= 3000)
+			return true;
+		else
+			return false;
 	}
 
+	public Player() {
+		account = new Account();
+		name = "";
 
+	}
 
+	// returns the name of the player
+	public String getName() {
+		return name;
+	}
+
+	// returns the money the player has
+	public int getBalance() {
+		return account.getBalance();
+	}
+
+	// adds money to the player account
+	public void addBalance(int value) {
+		account.addBalance(value);
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
