94a67e8ad96a9eee90c436829ac21f4d8c18b148
Casper <Casper@Casper.clients.wireless.dtu.dk>
2017-11-06 14:32:37 +0100

    Filled in all methods and wrote descriptions for them

--- src/cdio2_game/Cup.java
+++ src/cdio2_game/Cup.java
@@ -1,55 +1,48 @@
 package cdio2_game;
 
-
-import java.util.Random;
-
 public class Cup {
-	private int[] eyes = new int[3];
-	private Random dice1,dice2;
-	private int sides;
-	
 	/**
-	 * 
-	 * @param sides
+	 * eyes[] is an array where eyes[1 to eyes.length-1] is a die in a cup. eyes[0] is the total sum of the rolled dice.
+	 * sides is the number of sides on every die. The standard die used here is a 6-sided die.
 	 */
-	public Cup(int sides){
-		this.sides = sides;
-		dice1 = new Random();
-		dice2 = new Random();
-	}
+	private int[] eyes = new int[3];
+	private int sides = 6;
 
 	/**
-	 * 
-	 * @return
+	 * 	constructor of the class Cup
+	 */
+	public Cup(){
+
+	}
+	/**
+	 * Method that rolls a die and returns the value
+	 * (Math.random) gives us a number from 0 to 0.9999999999999...
+	 * That number is multiplied with the number of sides on the die, with sides 6 the result ranges from 0 to 5.99999999999994...
+	 * 1 is added to that number, the result ranges from 1 to 6.99999999999994...
+	 * (int) removes all numbers after the comma, the result can only be 1, 2, 3, 4, 5 and 6.
+	 */
+	private int Dice() {
+		return (int)(Math.random()*sides+1);
+	}
+	/**
+	 * Method that rolls a number of dice and returns the sum of all dice.
+	 * eyes[0] is the place in the array that contains the total sum. Set to 0, to wipe previous sum.
+	 * I a for-loop all dice are rolled one at a time and their value is added to the total.
+	 * The total is then returned.
 	 */
 	public int rollCup() {
-		rollDices();
-		eyes[2] = eyes[0] + eyes[1];
-		return eyes[2];
+		eyes[0] = 0;
+		for(int i = 1; i < eyes.length; i++) {
+			eyes[i] = Dice();
+			eyes[0] += eyes[i];
+		}
+		return eyes[0];
+
 	}
-	
 	/**
-	 * 
-	 * @param number
-	 * @return
+	 * Return a number based on what input is placed instead of "number"
 	 */
 	public int getEyes(int number) {
 		return eyes[number];
 	}
-
-	/**
-	 * 
-	 */
-	private void rollDices() {
-		eyes[0] = dice1.nextInt(sides) + 1;
-		eyes[1] = dice2.nextInt(sides) + 1;
-	}
-
-	/**
-	 * 
-	 */
-	public String toString() {
-		return "( " + eyes[0] + " + " + eyes[1] + " ) = " + eyes[2] + ".";
-	}
 }
-
