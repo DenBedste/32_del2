0183f165fe1da9aca7de470b822da694e790defe
s175219 <s175219@DTU-CZC6268CJY.win.dtu.dk>
2017-11-06 12:14:21 +0100

    Added corrected player registration, and made switch-cases for players
    Corrected the player registration with proper balance management, and
    made switch-cases which refer to the localisations, and adds to balance.

--- src/cdio2_game/Launcher.java
+++ src/cdio2_game/Launcher.java
@@ -17,22 +17,135 @@ public class Launcher {
 		res = ResourceBundle.getBundle("localisation.MessagesBundle");
 		System.out.println(res.getString("Tower"));
 
-		Player player1, player2;
-		Account account1, account2;
+		int balance = 1000;
 		String Name;
-		
+		// fix this pls
 		Scanner sc = new Scanner(System.in);	
-		System.out.println("Enter name of first user");
+		System.out.println(res.getString("1stplayer"));
 		Name = sc.nextLine();
-		account1 = new Account(1000);
-		player1 = new Player(Name, account1);
+		Player player1 = new Player(Name, balance);
+
+		System.out.println(res.getString("2ndplayer"));
+		Name = sc.nextLine();
+		Player player2 = new Player(Name, balance);
+
+		while(true) {
+
+
+			System.out.println(res.getString("roll.dices"));
+			Cup.rollCup();
+
+			int eyes = Cup.getEyes(eyes);
+			switch(eyes) {
+			case 2:
+				System.out.println(res.getString("FieldText2"));
+				player1.addToBalance(250);
+				break;
+			case 3:
+				System.out.println(res.getString("FieldText3"));
+				player1.addToBalance(-100);
+				break;
+			case 4:
+				System.out.println(res.getString("FieldText4"));
+				player1.addToBalance(100);
+				break;
+			case 5:
+				System.out.println(res.getString("FieldText5"));
+				player1.addToBalance(-20);
+				break;
+			case 6:
+				System.out.println(res.getString("FieldText6"));
+				player1.addToBalance(180);
+				break;
+			case 7:
+				System.out.println(res.getString("FieldText7"));
+				break;
+			case 8:
+				System.out.println(res.getString("FieldText8"));
+				player1.addToBalance(-70);
+				break;
+			case 9:
+				System.out.println(res.getString("FieldText9"));
+				player1.addToBalance(60);
+				break;
+			case 10:
+				System.out.println(res.getString("FieldText10"));
+				player1.addToBalance(-80);
+				//Add extra turn feature!
+				break;
+			case 11:
+				System.out.println(res.getString("FieldText11"));
+				player1.addToBalance(-50);
+				break;
+			case 12:
+				System.out.println(res.getString("FieldText12"));
+				player1.addToBalance(650);
+				break;	
+			}
+			if(player1.balance >= 3000) {
+				System.out.println(res.getString("congrats"));
+				System.out.println("");
+				System.out.println(res.getString("thanks"));
+				break;
+			}
+		}
+		System.out.println(res.getString("roll.dices"));
+		Cup.rollCup();
+
+		int eyes = Cup.getEyes(eyes);
+		switch(eyes) {
+		case 2:
+			System.out.println(res.getString("FieldText2"));
+			player2.addToBalance(250);
+			break;
+		case 3:
+			System.out.println(res.getString("FieldText3"));
+			player2.addToBalance(-100);
+			break;
+		case 4:
+			System.out.println(res.getString("FieldText4"));
+			player2.addToBalance(100);
+			break;
+		case 5:
+			System.out.println(res.getString("FieldText5"));
+			player2.addToBalance(-20);
+			break;
+		case 6:
+			System.out.println(res.getString("FieldText6"));
+			player2.addToBalance(180);
+			break;
+		case 7:
+			System.out.println(res.getString("FieldText7"));
+			break;
+		case 8:
+			System.out.println(res.getString("FieldText8"));
+			player2.addToBalance(-70);
+			break;
+		case 9:
+			System.out.println(res.getString("FieldText9"));
+			player2.addToBalance(60);
+			break;
+		case 10:
+			System.out.println(res.getString("FieldText10"));
+			player2.addToBalance(-80);
+			//Add extra turn feature!
+			break;
+		case 11:
+			System.out.println(res.getString("FieldText11"));
+			player2.addToBalance(-50);
+			break;
+		case 12:
+			System.out.println(res.getString("FieldText12"));
+			player2.addToBalance(650);
+			break;
+		}
+		if(player2.balance >= 3000) {
+			System.out.println(res.getString("congrats"));
+			System.out.println("");
+			System.out.println(res.getString("thanks"));
+			break;
+		}
 		
-		System.out.println("Enter name of the second user");
-		Name = sc.nextLine();
-		account2 = new Account(1000);
-		player2 = new Player(Name, account2);
-
-
 
 		sc.close();
 	}
